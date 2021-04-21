d8ee2ad68c3057e970f1b66ce7d574991480984c
mccabedi@bc.edu <mccabedi@bc.edu>
2021-04-20 21:55:59 -0400

    Implements both functions

--- src/main/scala/project_3/main.scala
+++ src/main/scala/project_3/main.scala
@@ -17,14 +17,247 @@ object main{
   Logger.getLogger("org.spark-project").setLevel(Level.WARN)
 
   def LubyMIS(g_in: Graph[Int, Int]): Graph[Int, Int] = {
+    var remaining_vertices = g_in.numVertices
+    var g = g_in
     while (remaining_vertices >= 1) {
-        // To Implement
+
+      val degree_rdd = g.aggregateMessages[(Int, Int)]( 
+      triplet => {
+        if (triplet.srcAttr != -1 && triplet.srcAttr != 1)
+        {
+          if (triplet.dstAttr != -1 && triplet.dstAttr != 1)
+          {
+            triplet.sendToDst((1, 0))
+            triplet.sendToSrc((1, 0))
+          }
+          else {
+            triplet.sendToSrc((0, 0))
+            triplet.sendToDst((-1, triplet.dstAttr))
+          }
+        }
+        else if (triplet.dstAttr != -1 && triplet.dstAttr != 1){
+          triplet.sendToDst((0, 0))
+          triplet.sendToSrc((-1, triplet.srcAttr))
+        }
+        else 
+        {
+          triplet.sendToSrc((-1, triplet.srcAttr))
+          triplet.sendToDst((-1, triplet.dstAttr))
+        }
+    }, ((a, b) => {
+      if (a._1 >= 0) {
+        (a._1 + b._1, 0)
+      }
+      else {
+        a
+      }
+    }) )
+    val r = new scala.util.Random()
+    val randbit_rdd = degree_rdd.map(x => {
+      if (x._2._1 == 0) 
+      {
+        (x._1, (x._2._1, 1, x._2._2))
+      }
+      if (x._2._1 < 0) 
+      {
+        (x._1, (x._2._1, 0, x._2._2))
+      }
+      val f = r.nextFloat
+      if (f < 1.0/(2*x._2._1))
+      {
+        (x._1, (x._2._1, 1, x._2._2))
+      }
+      else
+      {
+        (x._1, (x._2._1, 0, x._2._2))
+      }
+    })
+    val g_new = Graph(randbit_rdd, g.edges)
+    // val g_finished only has activated vertices
+    val updates_rdd = g_new.aggregateMessages[Int](
+      triplet => {
+        if (triplet.srcAttr._3 != 0 && triplet.dstAttr._3 != 0)
+        {
+          if (triplet.srcAttr._3 == 1)
+          {
+            triplet.sendToSrc(0)
+          }
+          else 
+          {
+            triplet.sendToSrc(-1)
+          }
+          if (triplet.dstAttr._3 == 1)
+          {
+            triplet.sendToDst(0)
+          }
+          else 
+          {
+            triplet.sendToDst(-1)
+          }
+        }
+        else if (triplet.srcAttr._3 != 0)
+        {
+          if (triplet.srcAttr._3 == 1)
+          {
+           triplet.sendToSrc(0)
+          }
+          else 
+          {
+            triplet.sendToSrc(-1)
+          }
+          if (triplet.dstAttr._2 == 1)
+          {
+            triplet.sendToDst(0)
+          }
+          else
+          {
+            triplet.sendToDst(1)
+          }
+        }
+        else if (triplet.dstAttr._3 != 0)
+        {
+          if (triplet.dstAttr._3 == 1)
+          {
+            triplet.sendToDst(0)
+          }
+          else 
+          {
+            triplet.sendToDst(-1)
+          }
+          if (triplet.srcAttr._2 == 1)
+          {
+            triplet.sendToSrc(0)
+          }
+          else
+          {
+            triplet.sendToSrc(1)
+          }
+        }
+        else
+        {
+          if (triplet.srcAttr._2 == 1 && triplet.dstAttr._2 == 1)
+          {
+            if (triplet.srcAttr._1 >= triplet.dstAttr._1)
+            {
+              triplet.sendToSrc(0)
+              triplet.sendToDst(1)
+            }
+            else
+            {
+              triplet.sendToSrc(1)
+              triplet.sendToDst(0)
+            }
+          }
+          else if (triplet.srcAttr._2 == 1)
+          {
+            triplet.sendToSrc(0)
+            triplet.sendToDst(1)
+          }
+          else if (triplet.dstAttr._2 == 1)
+          {
+            triplet.sendToSrc(1)
+            triplet.sendToDst(0)
+          }
+          else
+          {
+            triplet.sendToSrc(1)
+            triplet.sendToDst(1)
+          }
+        }}, ((a, b) => {
+        if (a < 0)
+        {
+          -1
+        }
+        else
+        {
+          a+b
+        }}) ).map(x => {
+        if (x._2 == 0)
+        {
+          (x._1, 1)
+        }
+        else if (x._2 == -1)
+        {
+          x
+        }
+        else
+        {
+          (x._1, 0)
+        }} )
+      val g_almost = Graph(updates_rdd, g_in.edges)
+      val remove_neighbors = g_almost.aggregateMessages[Int](
+      (triplet => {
+        if (triplet.srcAttr == 1)
+        {
+          triplet.sendToDst(-1)
+          triplet.sendToSrc(1)            
+        }
+        else if (triplet.dstAttr == 1)
+        {
+          triplet.sendToSrc(-1)
+          triplet.sendToDst(1)
+        }
+        else
+        {
+          triplet.sendToSrc(0)
+          triplet.sendToDst(0)
+        }
+      }), ((a, b) => {
+        if (a != 0)
+        {
+          a
+        }
+        else          
+        {
+          b
+        }}) )
+    val g_final = Graph(remove_neighbors, g_in.edges)
+    g = g_final
+
+    remaining_vertices = g.vertices.map(x => {
+    if (x._2 == 0)
+    {
+      1
     }
+    else
+    {
+      0
+    }
+    }).collect.sum
+    println(remaining_vertices)
+  }
+    return g
   }
 
 
   def verifyMIS(g_in: Graph[Int, Int]): Boolean = {
-    // To Implement
+
+    // Checks for vertices not next to an active one
+    val rdd1 = g_in.aggregateMessages[Int]( 
+      triplet => {
+        if (triplet.srcAttr != -1 || triplet.dstAttr != -1) {
+          triplet.sendToDst(1)
+          triplet.sendToSrc(1)
+        }
+    }, ((a, b) => 1) )
+    val bordered = rdd1.map(x => x._2).collect.sum
+
+    // Checks for activate vertices next to one another
+    val collisions_rdd = g_in.triplets.map(x => {
+      if (x.srcAttr == 1 && x.dstAttr == 1)
+      {
+        1
+      }
+      else 
+      {
+        0
+      }
+    })
+
+    if (collisions_rdd.reduce(_+_) > 0 || bordered.toLong != g_in.numVertices){
+      return false
+    }
+    return true
   }
 
 
@@ -45,7 +278,7 @@ object main{
         sys.exit(1)
       }
       val startTimeMillis = System.currentTimeMillis()
-      val edges = sc.textFile(args(1)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} )
+      val edges = sc.textFile(args(1)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} ).filter({case Edge(a,b,c) => a!=b})
       val g = Graph.fromEdges[Int, Int](edges, 0, edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
       val g2 = LubyMIS(g)
 
@@ -64,7 +297,7 @@ object main{
         sys.exit(1)
       }
 
-      val edges = sc.textFile(args(1)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} )
+      val edges = sc.textFile(args(1)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} ).filter({case Edge(a,b,c) => a!=b})
       val vertices = sc.textFile(args(2)).map(line => {val x = line.split(","); (x(0).toLong, x(1).toInt) })
       val g = Graph[Int, Int](vertices, edges, edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
 
